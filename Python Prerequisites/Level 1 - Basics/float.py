80d5aa47a94998b00e2f34129f5b54b6a0e0ae3f
Lukesh M <lukeshmurugesan@gmail.com>
2026-09-17 22:33:00 +0530

    Add float operations examples in float.py
    Introduced examples demonstrating float operations, including division, average calculation, and total price calculation.

---
+++ Python Prerequisites/Level 1 - Basics/float.py
@@ -0,0 +1,38 @@
+# float - Decimal Numbers
+
+price = 99.89
+height = 175.5
+average = 82.2
+
+a = 10
+b = 6
+print(a/b) #1.666666666666
+# / normally produces float
+
+# eg : Average
+total = 450
+count = 5
+
+percentage = total / count
+print(percentage) #90.0
+
+#1. price = 99.50, quantity = 3, Calculate the total price.
+
+price = 99.50
+quantity = 3
+total_price = price * quantity
+print(total_price) # 298.5
+
+#2. a = 10. b = 4, Calculate the average of a and b.
+a = 10
+b = 4
+average = a / b
+print(average) #2.5
+
+#3. What is the difference between: 10 / 3 and 10 // 3
+
+a = 10
+b = 3
+
+print(a / b) # 3.333333333333333 - / performs division and returns in decimal value
+print(a // b) # 3 - floor division that gives round value of nearest number
