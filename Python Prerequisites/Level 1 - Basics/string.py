4ae1b754c9dc24aaae9064ac2dff333d7f4bee61
Lukesh M <lukeshmurugesan@gmail.com>
2026-09-17 22:50:28 +0530

    Create string.py with string manipulation examples
    Added a string.py file with examples of string operations, including indexing, length calculation, concatenation, and formatted strings.

---
+++ Python Prerequisites/Level 1 - Basics/string.py
@@ -0,0 +1,53 @@
+# str - string
+
+name = "Alex"
+city = "San Francisco"
+department = "Artificial Intelligence"
+
+# we can access characters by index
+
+name = "Alex"
+print(name[0]) #A
+print(name[-1]) #x
+
+# length of string- len()
+
+print(len(name)) # 4
+
+# string concatenation
+
+first_name = "Will"
+last_name = "Smith"
+
+full_name = first_name + " " + last_name
+print(full_name)
+
+# f-string
+
+print(f"My name is {name} and I live in {city}")
+
+# Exercises
+
+# 1.name = "Alexa" , Print: first character, last character, length
+
+name = "Alexa"
+print(name[0]) #A
+print(name[-1]) #a
+print(len(name)) #5
+
+# 2. word = "Python", Print: p y t h o n Vertically
+
+word = "Python"
+
+print(word[0]) #P
+print(word[1]) #y
+print(word[2]) #t
+print(word[3]) #h
+print(word[4]) #o
+print(word[5]) #n
+
+
+
+
+
+
