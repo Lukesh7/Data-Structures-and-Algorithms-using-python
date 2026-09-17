f7092e725326430beec6e018a851f682aade719f
Lukesh M <lukeshmurugesan@gmail.com>
2026-09-17 22:08:51 +0530

    Add files via upload

---
+++ Python Prerequisites/Level 1 - Basics/integer.py
@@ -0,0 +1,44 @@
+#Integer - stores whole number
+"""
+age = 21
+marks = 85
+count = 100
+temperature = -5
+
+print(f"Age of the student is {age}")
+print(f"Marks of the exam is {marks}")
+print(f"Count begins from {count}")
+print(f"The temperature of the food is {temperature}")
+"""
+# Operators
+#(- Subtraction, + Addition , * Multiplication, / Division, // Floor Division, % Remainder, ** Power)
+
+#1.a = 25 , b = 7 , Find: addition, subtraction, multiplication, remainder, floor division
+
+a = 25
+b = 7
+
+print(a + b) #32
+print(a - b) #18
+print(a * b) #175
+print(a / b) #3.5714285714285716
+print(a // b) #3
+print(a % b) #4
+print(a ** b) #6103515625
+
+
+#2. Create a variable marks = 85 and add 10 to it.
+
+marks = 85
+marks = marks + 10
+print(marks) #95
+
+#3. Create count = 0. Increase it by 1 three times.
+
+count = 0
+count += 1
+count += 1
+count += 1
+print(count)
+
+
