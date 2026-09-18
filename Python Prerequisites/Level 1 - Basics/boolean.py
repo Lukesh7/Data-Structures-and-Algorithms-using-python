393db9aae17da07c3218ce35b1dbca599bc2b7cf
Lukesh M <lukeshmurugesan@gmail.com>
2026-09-18 14:53:16 +0530

    Create boolean.py with boolean examples and exercises
    Added a boolean.py file demonstrating boolean operations and comparisons in Python, including exercises for practice.

---
+++ Python Prerequisites/Level 1 - Basics/boolean.py
@@ -0,0 +1,45 @@
+#bool - boolean - True and False
+
+age = 15
+print(age > 18) #False
+
+a = 20
+b = 50
+
+print(a == b) #equal to
+print(a != b) # not equal
+print(a > b) # a greater than b
+print(a < b) # a lesser than b
+print(a >= b) # a greater than or equals to b
+print(a <= b) # a lesser than or equals to b
+
+# DSA use of bool
+
+found = False
+if found:
+    print("founded")
+
+#Exercises
+#1.age = 20. Check whether age is greater than or equal to 18.
+age = 20
+if age >= 18:
+    print("Yes")
+
+#2.a = 10, b = 20. Check: a == b, a != b, a < b, a > b
+a = 10
+b = 20
+
+print(a == b) #False
+print(a != b) #True
+print(a < b) #True
+print(a > b) #False
+
+#3.number = 15. Create a boolean variable that tells whether the number is greater than 10.
+
+number = 15
+is_greater = number > 10
+print(is_greater) #True
+
+
+
+
