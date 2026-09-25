6274fc14575a94558e14680cd17bd1ae05fa76fc
Lukesh M <lukeshmurugesan@gmail.com>
2026-09-25 21:22:09 +0530

    Create ifelseconditions.py with age eligibility checks
    Added example of if-else conditions in Python.

---
+++ Python Prerequisites/Level 1 - Basics/ifelseconditions.py
@@ -0,0 +1,14 @@
+#if-else conditions
+
+#if condition
+
+age = 19
+if age >= 18:
+    print("Eligible")
+
+#with else condition
+
+if age >= 18:
+    print('Eligible')
+else:
+    print('Not Eligible')
