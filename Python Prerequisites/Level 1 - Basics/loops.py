671cd0e2eae93945c3d41b4756a16aa7302a5b3e
Lukesh M <lukeshmurugesan@gmail.com>
2026-09-25 21:26:03 +0530

    Create loops.py with for loop examples
    Add examples of for loops and list iteration.

---
+++ Python Prerequisites/Level 1 - Basics/loops.py
@@ -0,0 +1,25 @@
+#loops
+
+#for loop
+for i in range(5):
+    print(i)
+"""
+0
+1
+2
+3
+4
+"""
+
+# loops through list
+numbers = [3,4,5,6]
+
+for num in numbers:
+    print(num)
+
+"""
+3
+4
+5
+6
+"""
